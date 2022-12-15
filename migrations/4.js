125e55485234e5de4adb32bcacbad02056373f75
kumarbikas521 <kumarbikas521@gmail.com>
2022-12-15 21:23:47 +0530

    Adding new content type

--- migrations/4.js
+++ migrations/4.js
@@ -1,4 +1,4 @@
 module.exports = function runMigration(migration) {
-    const test = migration.createContentType('Test');
+    const test = migration.createContentType('Test').name('Test Content Model').description("Test model");
     test.createField('title').name('title').type('Symbol').required(false);
   };
